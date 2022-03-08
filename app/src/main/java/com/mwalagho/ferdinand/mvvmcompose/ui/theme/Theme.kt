51171e64b5c7381726968c1410eb67144a7ac1cd
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-03-08 15:32:11 +0300

    Rename

--- app/src/main/java/com/mwalagho/ferdinand/mvvmcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/mwalagho/ferdinand/mvvmcompose/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun MVVMComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun KoolCryptoTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
