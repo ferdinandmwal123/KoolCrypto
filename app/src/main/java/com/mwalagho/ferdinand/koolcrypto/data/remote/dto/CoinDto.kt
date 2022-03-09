68412638c8b4c745b7344a806855bcf3b08a5da8
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-03-09 13:12:24 +0300

    Completed DTO's, interface and map function

---
+++ app/src/main/java/com/mwalagho/ferdinand/koolcrypto/data/remote/dto/CoinDto.kt
@@ -0,0 +1,24 @@
+package com.mwalagho.ferdinand.koolcrypto.data.remote.dto
+
+import com.mwalagho.ferdinand.koolcrypto.domain.model.Coin
+
+data class CoinDto(
+    val id: String,
+    val is_active: Boolean,
+    val is_new: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+    val type: String
+)
+
+fun CoinDto.toCoin() : Coin {
+    return Coin(
+        id = id,
+        is_active = is_active,
+        name = name,
+        rank = rank,
+        symbol = symbol
+    )
+
+}
--- app/src/main/java/com/mwalagho/ferdinand/koolcrypto/data/remote/dto/CoinPaprikaAPI.kt
+++
@@ -1,8 +0,0 @@
-package com.mwalagho.ferdinand.koolcrypto.data.remote.dto
-
-import retrofit2.http.GET
-
-interface CoinPaprikaAPI {
-
-    @GET()
-}
