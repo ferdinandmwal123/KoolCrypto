0f2a5b3d1930b5265319e5183cb2711449396863
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-03-11 14:34:09 +0300

    COmpleted DI with hilt

--- app/src/main/java/com/mwalagho/ferdinand/koolcrypto/CoinApplication.kt
+++ app/src/main/java/com/mwalagho/ferdinand/koolcrypto/CoinApplication.kt
@@ -1,4 +1,7 @@
 package com.mwalagho.ferdinand.koolcrypto
 
-class CoinApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication : Application()
