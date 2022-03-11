661d6bd01ee4cc322cd6a5fb79adb768352e924a
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-03-11 14:27:46 +0300

    injected coin paprika api interface

--- app/src/main/java/com/mwalagho/ferdinand/koolcrypto/common/Constants.kt
+++ app/src/main/java/com/mwalagho/ferdinand/koolcrypto/common/Constants.kt
@@ -2,4 +2,5 @@ package com.mwalagho.ferdinand.koolcrypto.common
 
 object Constants{
 
+    const val BASE_URL = "https://api.coinpaprika.com/"
 }
--- app/src/main/java/com/mwalagho/ferdinand/koolcrypto/di/AppModule.kt
+++ app/src/main/java/com/mwalagho/ferdinand/koolcrypto/di/AppModule.kt
@@ -1,11 +1,34 @@
 package com.mwalagho.ferdinand.koolcrypto.di
 
+import com.mwalagho.ferdinand.koolcrypto.common.Constants
+import com.mwalagho.ferdinand.koolcrypto.data.remote.CoinPaprikaAPI
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
+/*
+* Component 	       Injector for
+* SingletonComponent 	Application
+* */
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaAPI {
+
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaAPI::class.java)
+
+    }
+
+    @
 }
