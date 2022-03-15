cc9e5efa3f58f3c014086d931ace57c8b3be4b61
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-03-15 13:27:01 +0300

    hilt dependencies gettingmout of hand

---
+++ app/src/main/java/com/mwalagho/ferdinand/koolcrypto/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,12 @@
+package com.mwalagho.ferdinand.koolcrypto.presentation.coin_list
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.hilt.navigation.compose.hiltViewModel
+
+
+@Composable
+fun CoinListScreen(navController: NavController, viewModel: CoinListViewModel = hiltViewModel()
+) {
+    
+}
