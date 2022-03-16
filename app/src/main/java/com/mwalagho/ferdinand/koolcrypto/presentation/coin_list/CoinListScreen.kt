a28914527118425eb1d1ac4812ebc3bf2cf8cded
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-03-16 14:20:46 +0300

    Error and Navigation class

--- app/src/main/java/com/mwalagho/ferdinand/koolcrypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/mwalagho/ferdinand/koolcrypto/presentation/coin_list/CoinListScreen.kt
@@ -4,23 +4,36 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.mwalagho.ferdinand.koolcrypto.presentation.Screen
 import com.mwalagho.ferdinand.koolcrypto.presentation.coin_list.components.CoinListItem
 
 
 @Composable
-fun CoinListScreen(navController: NavController, viewModel: CoinListViewModel = hiltViewModel()
+fun CoinListScreen(
+    navController: NavController, viewModel: CoinListViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    Box(modifier = Modifier.fillMaxSize()){
-        LazyColumn(modifier = Modifier.fillMaxSize()){
-            items(state.coins){ coin ->
-                CoinListItem(coin = coin, onItemClicked = )
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.coins) { coin ->
+                CoinListItem(coin = coin, onItemClicked = {
+                    navController.navigate(Screen.CoinDetailScreen.route + "/${it.id}")
+                }
+                )
             }
         }
+
+        if(state.error.isNotBlank()) {
+            Text(text = state.error, color = MaterialTheme.colors.error, textAlign = TextAlign.Center)
+        }
     }
-    
+
+
 }
